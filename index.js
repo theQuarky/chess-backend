062544dc3eefefc812eecfbb9629122fb54e6016
ranahiren27 <ranahiren27@gmail.com>
2023-07-05 13:15:41 +0530

    changes

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const http = require("http").Server(app);
+
+const rooms = {};
+
 const io = require("socket.io")(http, {
   cors: {
     origin: "*",
@@ -17,16 +20,36 @@ http.listen(process.env.PORT || 4000, () =>
 
 io.on("connection", (socket) => {
   console.log("connected...");
+
   socket.on("join-room", ({ roomName, userId }) => {
     console.log("join-room", roomName, userId);
     socket.join(roomName);
+    if (!rooms[roomName]) {
+      rooms[roomName] = {
+        users: [userId],
+      };
+    } else {
+      if(!rooms[roomName].users.length >= 2) {
+        socket.emit("room-full", "Room is full");
+        return;
+      }
+      rooms[roomName].users.push(userId);
+    }
+        
     socket.to(roomName).emit("user-joined", "User joined your invitation");
   });
 
-  socket.on("send-move", ({ roomName, userId, move, turn }) => {
-    console.log("send-move", { roomName, userId, move });
+  socket.on("send-move", ({ roomName, userId, move, turn, gameState }) => {
+    console.log("send-move", { roomName, userId, move, gameState });
+
+    // saving game state
+    rooms[roomName].gameState = gameState;
+
+    console.log("rooms", rooms);
+
     socket
       .to(roomName)
       .emit("receive-move", { roomName, userId, move, yourTurn: turn });
   });
+
 });
